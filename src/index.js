ec67612c8b711ad9c368dab82b6d237a7cb7cc60
Yuna <yuna00708@gmail.com>
2024-12-31 22:46:17 +0900

    :art: refactor: msw initialization

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import './styles/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
